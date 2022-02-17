33d5c6c0235b81d0e5169486c4d72de5fe517073
Lawrence Buta <larry.buta@gmail.com>
2022-02-17 18:46:30 -0500

    Using The Task Class Start Method-02/17/2022

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskBasedAsynchronousProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread : {Thread.CurrentThread.ManagedThreadId} Started");
+            Task task1 = new(PrintCounter);
+            task1.Start();
+            Console.WriteLine($"Main Thread : {Thread.CurrentThread.ManagedThreadId} Completed");
+            Console.ReadKey();
+        }
+
+        static void PrintCounter()
+        {
+            Console.WriteLine($"Child Thread : {Thread.CurrentThread.ManagedThreadId} Started");
+            for (int count = 1; count <= 5; count++)
+            {
+                Console.WriteLine($"count value: {count}");
+            }
+            Console.WriteLine($"Child Thread : {Thread.CurrentThread.ManagedThreadId} Completed");
+        }
+    }
+}
